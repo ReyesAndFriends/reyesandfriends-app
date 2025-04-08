2460cef80e8e1044396760df0e38cc551f02c681
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-08 10:28:10 -0400

    feat: added SSL support for Flask mailer.

--- back/reyesandfriends-back/config.py
+++ back/reyesandfriends-back/config.py
@@ -8,5 +8,6 @@ class Config:
     MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.example.com')
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True').lower() in ['true', '1', 'yes']
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False').lower() in ['true', '1', 'yes']
     MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'your_email@example.com')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'your_password')
