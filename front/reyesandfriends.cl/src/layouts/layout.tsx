f310d7b50d8f12029a849f36046ccd330d99b86d
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-07 22:16:51 -0400

    feat: add background gradient to layout component

--- front/reyesandfriends.cl/src/layouts/layout.tsx
+++ front/reyesandfriends.cl/src/layouts/layout.tsx
@@ -4,7 +4,7 @@ import Footer from './components/footer/footer';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-gray-100 to-gray-300">
             <Navbar />
             <main className="flex-1">
                 {children}
