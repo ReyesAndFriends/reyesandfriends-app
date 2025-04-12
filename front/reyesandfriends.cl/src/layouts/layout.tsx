df5e109b575f4a5b79698c15df67798e8b9e44d8
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-12 01:44:52 -0400

    feat: add background color to main content area in layout

--- front/reyesandfriends.cl/src/layouts/layout.tsx
+++ front/reyesandfriends.cl/src/layouts/layout.tsx
@@ -6,7 +6,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div className="flex flex-col min-h-screen bg-gradient-to-b from-gray-100 to-gray-300">
             <Navbar />
-            <main className="flex-1">
+            <main className="flex-1 bg-zinc-900">
                 {children}
             </main>
             <Footer />
