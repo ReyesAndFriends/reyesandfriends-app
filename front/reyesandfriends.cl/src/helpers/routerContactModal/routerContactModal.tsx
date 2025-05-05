467cc195f1950b28993f4f5f7874c1c119d59c63
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-05 01:48:59 -0400

    fix: improve styling and alignment in RouterContactModal component

--- front/reyesandfriends.cl/src/helpers/routerContactModal/routerContactModal.tsx
+++ front/reyesandfriends.cl/src/helpers/routerContactModal/routerContactModal.tsx
@@ -9,20 +9,20 @@ const RouterContactModal: React.FC<RouterContactModalProps> = ({ onConfirm, onCa
     return (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
             <div className="bg-zinc-800 text-white rounded-lg p-6 max-w-md w-full">
-                <h2 className="text-2xl font-bold mb-4 text-red-500">Alerta</h2>
+                <h2 className="text-2xl  mb-4 text-red-500 text-center">Alerta</h2>
                 <p className="mb-6">
                     Estás dentro de una página de formulario. Si sales, tu información podría perderse. ¿Qué deseas hacer?
                 </p>
                 <div className="flex space-x-4">
                     <button
                         onClick={onCancel}
-                        className="w-full bg-red-600 text-white font-bold py-2 rounded hover:bg-red-700 transition-colors"
+                        className="w-full bg-red-700 hover:bg-red-800 text-white px-6 py-3 rounded transition-colors"
                     >
                         Mantenerme en esta página
                     </button>
                     <button
                         onClick={onConfirm}
-                        className="w-full bg-green-600 text-white font-bold py-2 rounded hover:bg-green-700 transition-colors"
+                        className="w-full bg-yellow-600 hover:bg-yellow-700 text-white px-6 py-3 rounded transition-colors"
                     >
                         Salir
                     </button>
