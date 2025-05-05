bb8f0d7360735095b97f8126169ccbd6e4bcc519
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-04 20:31:12 -0400

    Refactor : Update images, adjust text styles, and remove unnecessary sections

--- front/reyesandfriends.cl/src/pages/about-us/aboutUs.tsx
+++ front/reyesandfriends.cl/src/pages/about-us/aboutUs.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Bot, Code, Rat } from "lucide-react";
 
 const AboutUs: React.FC = () => {
     return (
@@ -17,10 +16,11 @@ const AboutUs: React.FC = () => {
                     </div>
                     <div className="flex flex-col items-center">
                         <img 
-                            src="/img/crown_white.png" 
+                            src="/img/logo/crown_white.svg" 
                             className="max-w-[350px] h-auto"
+                            alt="reyes&friends_crown"
                         />
-                        <p className="text-white text-center mt-4">
+                        <p className="text-white text-l text-center mt-4">
                             Innovación y excelencia en cada proyecto.
                         </p>
                     </div>
@@ -33,13 +33,13 @@ const AboutUs: React.FC = () => {
                         Nuestra Misión es clara:
                     </h2>
                     <p className="text-center text-gray-300 mb-8 text-xl md:text-2xl leading-relaxed">
-                        Brindar soluciones tecnológicas de alta calidad que impulsen el crecimiento y la innovación de nuestros clientes, 
-                        con un enfoque en excelencia, creatividad y compromiso.
+                        "Brindar soluciones tecnológicas de alta calidad que impulsen el crecimiento y la innovación de nuestros clientes, 
+                        con un enfoque en excelencia, creatividad y compromiso".
                     </p>
                 </div>
             </section>
 
-            <section className="py-16 bg-black text-white">
+            <section className="py-16 text-white">
                 <div className="container mx-auto px-4 max-w-5xl grid grid-cols-1 md:grid-cols-2 items-center gap-8">
                     <div className="text-center md:text-left">
                         <h2 className="text-3xl font-bold mb-6 text-red-600">
@@ -52,47 +52,10 @@ const AboutUs: React.FC = () => {
                     </div>
                     <div className="flex justify-center">
                         <img
-                            src="/img/reyesandfriends.svg"
-                            alt="Team"
-                            className="rounded shadow-md max-w-full h-auto"
+                            src="/img/logo/logo_white_2.svg"
+                            alt="reyes&friends"
+                            className="max-w-[350px] h-auto"
                         />
-                    </div>
-                </div>
-            </section>
-
-            <section className="py-16 text-white">
-                <div className="container mx-auto px-4">
-                    <h2 className="text-3xl font-bold mb-12 text-center text-red-600">
-                        Nuestras Secciones
-                    </h2>
-                    <p className="text-center text-gray-300 mb-8 text-xl md:text-2xl leading-relaxed">
-                        En Reyes&Friends a parte de ofrecer servicios personalizados, también ofrecemos diversos servicios a la comunidad con el fin de contribuir, por esta razón hemos clasificado en secciones:
-                    </p>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                        <div className="bg-zinc-900 p-6 rounded-lg shadow-md text-center">
-                            <h3 className="text-xl font-bold mb-4 text-red-500 flex items-center justify-center gap-2">
-                                <Bot className="w-6 h-6" /> AbbyBot Team
-                            </h3>
-                            <p className="text-gray-300">
-                                Equipo de desarrollo dedicado a la creación de AbbyBot, un asistente virtual que mejora la experiencia del usuario y comunidades gestionando servidores de Discord.
-                            </p>
-                        </div>
-                        <div className="bg-zinc-900 p-6 rounded-lg shadow-md text-center">
-                            <h3 className="text-xl font-bold mb-4 text-red-500 flex items-center justify-center gap-2">
-                                <Code className="w-6 h-6" /> Sección para Open Source
-                            </h3>
-                            <p className="text-gray-300">
-                                Contribuimos al ecosistema de código abierto con proyectos innovadores y útiles para todos.
-                            </p>
-                        </div>
-                        <div className="bg-zinc-900 p-6 rounded-lg shadow-md text-center">
-                            <h3 className="text-xl font-bold mb-4 text-red-500 flex items-center justify-center gap-2">
-                                <Rat className="w-6 h-6" /> Sección MiceLab
-                            </h3>
-                            <p className="text-gray-300">
-                                Ayudamos en la elaboración de MiceLab, una herramienta para el gestionamiento de pruebas en software.
-                            </p>
-                        </div>
                     </div>
                 </div>
             </section>
