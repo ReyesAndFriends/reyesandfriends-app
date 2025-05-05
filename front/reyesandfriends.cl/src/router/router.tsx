c614aa8f6f2f48a2d398e0e52f3e65261abd9248
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-04 21:33:39 -0400

    refactor: added scroll top animation for transitions and smooth style

--- front/reyesandfriends.cl/src/router/router.tsx
+++ front/reyesandfriends.cl/src/router/router.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
+
 import Layout from "../layouts/layout";
 import Home from "../pages/home";
 import Portfolio from "../pages/portfolio";
@@ -9,36 +10,40 @@ import Services from "../pages/services/services";
 import AboutUs from "../pages/about-us/aboutUs";
 import QuotePage from "../pages/quote-project/quoteProject";
 
+const pageTransition = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.3 },
+};
+
+const AnimatedRoutes: React.FC = () => {
+    const location = useLocation();
+
+    const handleExitComplete = () => {
+        window.scrollTo(0, 0);
+    };
+
+    return (
+        <AnimatePresence mode="wait" onExitComplete={handleExitComplete}>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<motion.div {...pageTransition}><Home /></motion.div>} />
+                <Route path="/services/web-development" element={<motion.div {...pageTransition}><div>Programación web</div></motion.div>} />
+                <Route path="/services/bussiness-software" element={<motion.div {...pageTransition}><div>Software empresarial</div></motion.div>} />
+                <Route path="/services/promotional-web" element={<motion.div {...pageTransition}><div>Páginas web promocionales</div></motion.div>} />
+                <Route path="/services/ecommerce-web" element={<motion.div {...pageTransition}><div>Páginas E-Commerce</div></motion.div>} />
+                <Route path="/services" element={<motion.div {...pageTransition}><Services /></motion.div>} />
+                <Route path="/about" element={<motion.div {...pageTransition}><AboutUs /></motion.div>} />
+                <Route path="/portfolio" element={<motion.div {...pageTransition}><Portfolio /></motion.div>} />
+                <Route path="/contact" element={<motion.div {...pageTransition}><Contact /></motion.div>} />
+                <Route path="/quote-project" element={<motion.div {...pageTransition}><QuotePage /></motion.div>} />
+            </Routes>
+        </AnimatePresence>
+    );
+};
+
+
 const AppRouter: React.FC = () => {
-    
-    const pageTransition = {
-        initial: { opacity: 0 },
-        animate: { opacity: 1 },
-        exit: { opacity: 0 },
-        transition: { duration: 0.3 },
-    };
-
-    const AnimatedRoutes = () => {
-        const location = useLocation();
-
-        return (
-            <AnimatePresence mode="wait">
-                <Routes location={location} key={location.pathname}>
-                    <Route path="/" element={<motion.div {...pageTransition}><Home /></motion.div>} />
-                    <Route path="/services/web-development" element={<motion.div {...pageTransition}><div>Programación web</div></motion.div>} />
-                    <Route path="/services/bussiness-software" element={<motion.div {...pageTransition}><div>Software empresarial</div></motion.div>} />
-                    <Route path="/services/promotional-web" element={<motion.div {...pageTransition}><div>Páginas web promocionales</div></motion.div>} />
-                    <Route path="/services/ecommerce-web" element={<motion.div {...pageTransition}><div>Páginas E-Commerce</div></motion.div>} />
-                    <Route path="/services" element={<motion.div {...pageTransition}><Services /></motion.div>} />
-                    <Route path="/about" element={<motion.div {...pageTransition}><AboutUs /></motion.div>} />
-                    <Route path="/portfolio" element={<motion.div {...pageTransition}><Portfolio /></motion.div>} />
-                    <Route path="/contact" element={<motion.div {...pageTransition}><Contact /></motion.div>} />
-                    <Route path="/quote-project" element={<motion.div {...pageTransition}><QuotePage/></motion.div>} />
-                </Routes>
-            </AnimatePresence>
-        );
-    };
-
     return (
         <Router>
             <Layout>
